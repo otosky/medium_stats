9f374094e5fdbba9ef0527f532ca2fe1b3514d82
Oliver Tosky <olivertosky@Olivers-MacBook-Pro.local>
2020-04-01 14:10:07 -0400

    Add longform_description

--- setup.py
+++ setup.py
@@ -1,4 +1,11 @@
+import pathlib
 from setuptools import setup, find_packages
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
 
 setup(
     name='medium-stats',
@@ -8,7 +15,7 @@ setup(
             'medium-stats = medium_stats.__main__:main'
         ]
     },
-    packages=find_packages(),
+    packages=find_packages(exclude=("tests",)),
     install_requires = ['requests'],
     extras_require={
         'selenium': ['selenium', 'webdriver_manager']
@@ -16,6 +23,8 @@ setup(
     author="Oliver Tosky",
     author_email="olivertosky@gmail.com",
     description="CLI tool to fetch your Medium stats",
+    long_description=README,
+    long_description_content_type="text/markdown",
     keywords="Medium blog scraper analytics",
     license='GNU GPLv3'
 )
